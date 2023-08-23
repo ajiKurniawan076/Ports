142179997ca898450f7739bcc9f77d6d50812901
unknown <ajikurniawan076@gmail.com>
2023-08-23 12:06:41 +0700

    update 1

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -11,15 +11,19 @@ export const About = () => {
 
   return ( 
     <div className={`flex w-full mt-5 ${rendered?'bg-black/30 scake-100':'scale-[10%] bg-transparent'} sm:h-[400px] rounded-3xl duration-300 overflow-auto`}>
-      <div className="p-10 text-white/80 w-4/5 bg-black/30 rounded-s-3xl h-full overflow-auto">
-        <h3 className="">Hello</h3> 
-        <div className="mt-3 p-4 text-sm font-semibold">My Name is Aji Kurniawan,
+      <div className="p-10 text-white/80 w-full md:w-4/5 bg-black/30 rounded-s-3xl h-full overflow-auto">
+        
+        <div className="flex justify-center w-full md:hidden">
+        <img src="./src/img/oni.jpg" alt="My Profil Image" className="group w-[150px] h-[150px] rounded-full hover:scale-150 duration-300"></img>
+        </div>
+        <h3 className="text-center">Hello, </h3> 
+        <div className="mt-3 p-4 text-sm font-semibold text-center">My Name is Aji Kurniawan,
         <br/> I was born in Salatiga in 24th February 1996
-        <br />i've been living in my hometown since i was born til i graduate from my University at Satya Wacana Christian University in 24th February 2023
-        <br /><br />I got Bachelor Degree in Informatic Engineering with Application Developer as classification. i focused my study in website and desktop application.
+        <br />I've been living in my hometown since i was kid until i graduated from my University at Satya Wacana Christian University in 24th February 2023
+        <br /><br />I got Bachelor Degree in Informatic Engineering with Application Developer as my classification. I focused my study in website and desktop application development.
         <br />So, these are the programming language and framework that i've learnt so far:
         <br />
-        <div className="flex max-md:flex-col gap-10 justify-between mt-10">
+        <div className="flex max-md:flex-col gap-10 justify-between mt-10 text-start">
         <ul className="w-1/2">Desktop App
           <li className=" list-disc">Java Swing</li>
         </ul>
@@ -38,7 +42,7 @@ export const About = () => {
         </div>
         </div>
       </div>
-      <div className="mt-10 relative group w-1/5 flex justify-center h-fit">
+      <div className="mt-10 relative group w-1/5 justify-center h-fit hidden md:flex">
         <img src="./src/img/oni.jpg" alt="My Profil Image" className="group w-[100px] h-[100px] md:w-[200px] md:h-[200px] rounded-full hover:scale-150 duration-300"></img>
         <p className="absolute bottom-[-75px] md:bottom-[-90px] scale-0 group-hover:scale-100 bg-yellow-300/40 duration-300 rounded-lg p-2">Aji Kurniawan</p>
       </div>
